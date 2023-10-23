73e97f87f08ea0bc262aa8b3b232e49e9a344be6
Ars Vladimirov <grokhael@gmail.com>
2023-10-23 09:01:56 +0000

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Eyes Microcirculation"
 PROJECT_NAME_FULL: str = "Eyes Microcirculation: Medicine Dataset for Eyes Semantic Segmentation Task"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
